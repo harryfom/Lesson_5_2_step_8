a41abdd4faf2610c46928dc17ec5d2bd5d5926b9
FomeIV <harryfom@gmail.com>
2016-03-24 08:45:44 +0300

    5.2 Потоки байт – Шаг 8 end

--- src/ru/fomenko_iv/lesson5_2/step8/Main.java
+++ src/ru/fomenko_iv/lesson5_2/step8/Main.java
@@ -1,5 +1,9 @@
 package ru.fomenko_iv.lesson5_2.step8;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /*
  Напишите метод, читающий входной поток и вычисляющий контрольную сумму прочитанных данных.
 
@@ -7,7 +11,7 @@ package ru.fomenko_iv.lesson5_2.step8;
 
  Контрольная сумма представляет собой число типа int. Контрольная сумма пустого набора данных равна нулю.
  Контрольная сумма непустого набора данных вычисляется по следующей рекуррентной формуле:
- Cn+1=rotateLeft(Cn) xor bn+1Cn+1=rotateLeft(Cn) xor bn+1 , где CnCn — контрольная сумма первых n байт данных,
+ Cn+1=rotateLeft(Cn) xor bn+1, где Cn — контрольная сумма первых n байт данных,
  rotateLeft — циклический сдвиг бит числа на один бит влево (чтобы не изобретать велосипед,
  используйте Integer.rotateLeft), bnbn — n-ный байт данных.
 
@@ -20,8 +24,22 @@ package ru.fomenko_iv.lesson5_2.step8;
 
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("5.2 Потоки байт – Шаг 8");
+        byte[] test = new byte[]{0x33, 0x45, 0x01};
+        int checkSum;
+        try (ByteArrayInputStream is = new ByteArrayInputStream(test)){
+            checkSum = checkSumOfStream(is);
+        }
+        System.out.println(checkSum);
 
+    }
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        int retVal=0;
+        int byteFromStream;
+        while ((byteFromStream = inputStream.read()) != -1){
+            retVal = Integer.rotateLeft(retVal,1) ^ byteFromStream;
+        }
+        return retVal;
     }
 }
